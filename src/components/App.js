34f26dbb8ef8d0a019ea5e182949728dd99c8be7
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-03-02 10:39:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showPara: false
-    }
-  };
-
-  handleClick = () => {
-    this.setState({
-      showPara: true
-    })
+      renderPara: false
+    };
   }
+
+  handleButtonClick = () => {
+    this.setState({ renderPara: true });
+  };
 
   render() {
     return (
       <div id="main">
         { /* Do not remove this main div!! */ }
-        <button id="click" onClick={this.handleClick}>Click me!</button>
-        {this.state.showPara && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+        <button id="click" onClick={this.handleButtonClick}>
+          Click
+        </button>
+        {this.state.renderPara ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+          </p>
+        ) : null}
       </div>
     );
   }
